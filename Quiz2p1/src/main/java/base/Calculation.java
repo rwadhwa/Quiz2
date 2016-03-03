6b01e29031098056f4c0fb58b0f6e4843f9cee14
Raveena Wadhwa <rwadhwa@udel.edu>
2016-03-03 17:14:32 -0500

    cleaned

--- Quiz2p1/src/main/java/base/Calculation.java
+++ Quiz2p1/src/main/java/base/Calculation.java
@@ -8,26 +8,26 @@ public class Calculation
 	private double RepaymentAPR;
 	private double Term;
 
-	// This is a constructor that takes user input
+	// constructor that takes user input
 	public Calculation()
 	{
-		getInput();
+		getUserInput();
 	}
 
-	// Paramater values for variable values
+	// Parameter values
 	public Calculation(double initialTuition, 
 			double tuitionPercentageIncrease, 
 			double repaymentAPR, 
 			double loanTerm)
 	{
-		this.TuitionTotal = initialTuition;
-		this.PercentageIncrease = tuitionPercentageIncrease;
-		this.RepaymentAPR = RepaymentAPR;
-		this.Term = Term;
+		TuitionTotal = initialTuition;
+		PercentageIncrease = tuitionPercentageIncrease;
+		RepaymentAPR = RepaymentAPR;
+		Term = Term;
 	}
 
 	// Scanner gets input from user
-	public void getInput()
+	public void getUserInput()
 	{
 		Scanner input = new Scanner(System.in);
 		System.out.print("Initial Tuition:");
@@ -40,12 +40,7 @@ public class Calculation
 		Term = input.nextInt();
 		input.close();
 	}
-
-	/*
-	 * Calculation for the total cost of tuition
-	 * 
-	 * @return TotalCost - the amount of money needed to pay off the loan
-	 */
+//Calculates and returns total cost
 	public double calculateTotalCost()
 	{
 		double TotalCost = (TuitionTotal / 
@@ -54,23 +49,18 @@ public class Calculation
 		return TotalCost;
 	}
 
-	/*
-	 * Calculates the amount paid monthly for tuition
-	 * 
-	 * @return monthlyCost - monthly payment to pay off loan
-	 */
+	//Calculates the monthly cost w online formula
 	public double calculateMonthlyCost()
 	{
-		double m = (((RepaymentAPR + PercentageIncrease)/100)/12);
-		double MonthlyCost = (m*calculateTotalCost()*(Math.pow(1+m, Term)))/
-				((Math.pow(1+m, Term))-1);
+		double MonthlyCost = ((((RepaymentAPR + PercentageIncrease)/100)/12)*calculateTotalCost()*(Math.pow(1+(((RepaymentAPR + PercentageIncrease)/100)/12), Term)))/
+				((Math.pow(1+(((RepaymentAPR + PercentageIncrease)/100)/12), Term))-1);
 		return MonthlyCost;
 	}
 	
-	//Prints the total and monthly costs
-	public void calculateAndPrint()
+
+	public void Printcost()
 	{
-		System.out.printf("The total cost is: $%,.2f%n", calculateTotalCost());
-		System.out.printf("The monthly cost is: $%,.2f%n", calculateMonthlyCost());
+		System.out.printf("When calculated the total cost is $%,.2f%n", calculateTotalCost());
+		System.out.printf("When calculated the monthly cost is $%,.2f%n", calculateMonthlyCost());
 	}
 }
